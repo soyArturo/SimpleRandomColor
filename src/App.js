173ebfb0ea0419bb8ff5899ced33cf720eefb4db
Arturo Perez <arturo.perez@grupoexcel.com>
2020-07-18 12:28:15 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import randomcolor from "randomcolor";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      color: "",
+    };
+    this.increment = this.increment.bind(this);
+  }
+  increment() {
+    const newColor = randomcolor();
+    this.setState({ color: newColor });
+  }
+  render() {
+    return (
+      <div style={{ backgroundColor: this.state.color }} className="App">
+        <div className="Container">
+          <h1>Simple Random Color</h1>
+          <button onClick={this.increment}>I'm feeling lucky</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
